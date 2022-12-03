547981dc3608c5f1776d5cd9092cb4559beb408b
Aan-Triambudi <aan.argon123@gmail.com>
2022-12-03 22:58:10 +0700

    tambah file index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello World!<br>";
+
+$nama = "Aan Triambudi";
+$umur = 20;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Anggun dwi indah kasih";
+$umurAdik = 9;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+?>
