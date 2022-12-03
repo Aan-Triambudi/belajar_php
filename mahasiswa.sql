72b3eda63e601194b945c00fdd1725d51dc7c43a
Aan-Triambudi <aan.argon123@gmail.com>
2022-12-03 23:29:08 +0700

    belajar mysql

---
+++ mahasiswa.sql
@@ -0,0 +1,38 @@
+-- buat database fakultas
+CREATE DATABASE seal_fakultas;
+
+-- buat table jurusan
+CREATE TABLE jurusan (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    kode CHAR(4) NOT NULL,
+    nama VARCHAR(50) NOT NULL
+);
+
+-- buat table mahasiswa
+CREATE TABLE mahasiswa (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_jurusan INTEGER NOT NULL,
+    nim CHAR(8) NOT NULL,
+    nama VARCHAR(50) NOT NULL,
+    jenis_kelamin enum ('Laki-Laki', 'perempuan') NOT NULL,
+    tempat_lahir VARCHAR(50) NOT NULL,
+    tanggal_lahir DATE NOT NULL,
+    alamat VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_jurusan) REFERENCES jurusan (id)
+);
+
+-- masukin data jurusan
+insert into jurusan (kode,nama) values ('APBL','Kedokteran');
+insert into jurusan (kode,nama) values ('AKTS','Psikologi');
+
+-- masukin data mahasiswa
+insert into mahasiswa (id_jurusan, nim, nama, jenis_kelamin, tempat_lahir, tanggal_lahir, alamat)
+values (1,'30001000', 'Aan', 'laki-laki', 'Solo', '2001-07-12', 'Jl. Srikandi');
+insert into mahasiswa (id_jurusan, nim, nama, jenis_kelamin, tempat_lahir, tanggal_lahir, alamat)
+values (2,'20220002', 'Rohman', 'laki-laki', 'Malang', '1999-11-21', 'Jl. gatotkaca');
+
+-- update data mahasiswa
+update mahasiswa set alamat = "Jl. gatotkaca" where id = 2;
+
+-- delete data mahasiswa
+delete from mahasiswa where id=2;
